7081f00df29b5334d1b6f444504d36eab385c534
deepaksuryamohanamurali <deepaksurya265@outlook.com>
2020-04-16 12:32:32 +0530

    Test Commit Deepak

--- app/src/main/java/com/wiates/kumaraguru/SigninActivity.java
+++ app/src/main/java/com/wiates/kumaraguru/SigninActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 
 public class SigninActivity extends AppCompatActivity {
 
-    String hii;
+    String hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

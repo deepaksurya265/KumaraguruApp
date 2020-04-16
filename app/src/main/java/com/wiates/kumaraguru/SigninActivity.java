861d1c4b0cb6c8456b425a73fbd2c0f06fcbc254
Deepak Surya Mohana Murali <deepaksuryamohanamurali@Deepaks-MacBook-Pro.local>
2020-04-16 11:11:19 +0530

    Initial commit

---
+++ app/src/main/java/com/wiates/kumaraguru/SigninActivity.java
@@ -0,0 +1,27 @@
+package com.wiates.kumaraguru;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class SigninActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
+        setContentView(R.layout.activity_signin);
+    }
+
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,SignupActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+}
